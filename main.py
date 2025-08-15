9a8e211e48186e21fb0e1c07e8ad8647ede1bb32
iammessier <nahatajay6@gmail.com>
2025-08-16 00:18:26 +0530

    Add prepare base model pipeline and update configuration; remove obsolete data ingestion scripts

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from cnnClassifier.pipeline.data_ingestion_01 import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.prepare_base_model_02 import PrepareBaseModelTrainingPipeline
 from cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
-if __name__ == "__main__":
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-    except Exception as e:
-        logger.exception(e)
-        raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
